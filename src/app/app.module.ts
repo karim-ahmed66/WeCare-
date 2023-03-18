bc3b6536e1c9c50d1d1103cfa8e903f5e87d46a0
Ahmed Waleed <me@ahmedwaleed.net>
2023-03-18 14:59:34 +0200

    Changing Project Stracture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
