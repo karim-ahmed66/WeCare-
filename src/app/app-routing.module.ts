5821a2fd0240fc6573fb905bf30c85635e6c81f6
Hendawy <96398845+AbdoHendawy@users.noreply.github.com>
2023-03-19 18:37:40 +0200

    fix routing round two

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { DashboardHomeComponent } from './dashboard-doctor/dashboard-home/dashboard-home.component';
+import { AddPatientComponent } from './dashboard-doctor/add-patient/add-patient.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: "addPatient", component: AddPatientComponent }
+  ,
   {
     path: 'dashboard-home', component: DashboardHomeComponent,
     children: [{
